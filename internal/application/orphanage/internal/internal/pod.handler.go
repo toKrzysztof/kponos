22e1dedc641538390aac3529499122cacf50396a
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2026-02-14 16:37:48 +0100

    implement deployment, pod, statefulset handlers

--- internal/application/orphanage/internal/internal/pod.handler.go
+++ internal/application/orphanage/internal/internal/pod.handler.go
@@ -3,32 +3,46 @@ package resourceHandler
 import (
 	"context"
 
-	corev1 "k8s.io/api/core/v1"
+	core "github.com/toKrzysztof/kponos/internal/core/reference_analyzer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // PodHandler handles finding references to Secrets and ConfigMaps in Pod resources
 type PodHandler struct {
 	client.Client
+	referenceAnalyzer *core.ReferenceAnalyzer
 }
 
 // NewPodHandler creates a new PodHandler
 func NewPodHandler(client client.Client) *PodHandler {
 	return &PodHandler{
-		Client: client,
+		Client:            client,
+		referenceAnalyzer: core.NewReferenceAnalyzer(client),
 	}
 }
 
 // FindReferences finds all Pods that reference the given Secret or ConfigMap
 func (h *PodHandler) FindReferences(ctx context.Context, c client.Client, secretName, configMapName string, namespace string) ([]client.Object, error) {
 	var pods []client.Object
-	podList := &corev1.PodList{}
-	if err := c.List(ctx, podList, client.InNamespace(namespace)); err != nil {
-		return pods, err
+
+	// Find Secret references if secretName is provided
+	if secretName != "" {
+		secretRefs, err := h.referenceAnalyzer.FindReferencesForSecret(ctx, secretName, namespace, "Pod")
+		if err != nil {
+			return nil, err
+		}
+		pods = append(pods, secretRefs...)
 	}
-	
-	// TODO: Filter pods that reference the secret/configmap
-	
+
+	// Find ConfigMap references if configMapName is provided
+	if configMapName != "" {
+		configMapRefs, err := h.referenceAnalyzer.FindReferencesForConfigMap(ctx, configMapName, namespace, "Pod")
+		if err != nil {
+			return nil, err
+		}
+		pods = append(pods, configMapRefs...)
+	}
+
 	return pods, nil
 }
 
