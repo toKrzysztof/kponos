66f993f25537ad5cbb00619690db4155f20a8510
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2026-02-14 15:33:17 +0100

    implement FindSecretReferences method & remove unnecessary comments

--- internal/core/reference_analyzer/internal/ingress_reference_finder.go
+++ internal/core/reference_analyzer/internal/ingress_reference_finder.go
@@ -28,9 +28,8 @@ func (f *IngressReferenceFinder) FindSecretReferences(ctx context.Context, clien
 	return nil, nil
 }
 
-// FindConfigMapReferences finds all Ingresses that reference the given ConfigMap
+// Ingress does not reference ConfigMaps. This method is implemented to satisfy the ReferenceFinderStrategy interface.
 func (f *IngressReferenceFinder) FindConfigMapReferences(ctx context.Context, client client.Client, configMapName, namespace string) ([]client.Object, error) {
-	// TODO: Implement logic to find Ingresses referencing the configmap
 	return nil, nil
 }
 
